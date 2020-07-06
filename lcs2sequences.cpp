3e1ff83e9d24a0a942ecc344f06b78cee3cfae53
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-06 09:16:53 +0530

    WEEK 5

---
+++ lcs2sequences.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+int lcs( int *X, int *Y, int m, int n ) 
+{ 
+	int L[m + 1][n + 1]; 
+	int i, j; 
+	for (i = 0; i <= m; i++) 
+	{ 
+		for (j = 0; j <= n; j++) 
+		{ 
+		if (i == 0 || j == 0) 
+			L[i][j] = 0; 
+	
+		else if (X[i - 1] == Y[j - 1]) 
+			L[i][j] = L[i - 1][j - 1] + 1; 
+	
+		else
+			L[i][j] = max(L[i - 1][j], L[i][j - 1]); 
+		} 
+	} 
+	return L[m][n]; 
+} 
+
+int main() 
+{ 
+	int m;
+	cin>>m;
+	int X[m];
+	for(int i=0;i<m;i++)
+	cin>>X[i]; 
+	int n;
+	cin>>n;
+	int Y[n];
+	for(int i=0;i<n;i++)
+	cin>>Y[i];
+	cout << lcs( X, Y, m, n ); 
+
+} 
